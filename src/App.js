c1341c4fd4b93404b6434b61ce67fa654a61947f
khoon0608 <khoon0608@gmail.com>
2022-03-19 23:54:51 +0900

    훅을 사용하는 함수 컴포넌트 방식

--- src/App.js
+++ src/App.js
@@ -3,27 +3,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-class App extends React.Component {
-  state = {
-    count: 0,
-  };
-  modify = (n) => {
-    this.setState({
-      count: n,
-    });
-  };
-  render() {
-    const { count } = this.state;
-    return (
-      <div>
-        <span>{count}</span>
-        <button onClick={() => this.modify(count + 1)}>Increase</button>
-      </div>
-    );
-  }
+function App() {
+  const [count, setCount] = React.useState(0);
+
+  return (
+    <div>
+      {count}
+      <button onClick={() => setCount((prevCount) => prevCount + 1)}></button>
+    </div>
+  );
 }
 
-
-// 훅을 사용하기 전의 컴포넌트 코딩방식
+// 훅을 사용하는 현재 방식
 
 export default App;
