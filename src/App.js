b22792df0c3ca25c579ea4c330ed39953b480978
khoon0608 <khoon0608@gmail.com>
2022-03-20 10:29:42 +0900

    useTitle 적용

--- src/App.js
+++ src/App.js
@@ -4,27 +4,12 @@ import { validate } from "json-schema";
 import React from "react";
 import ReactDOM from "react-dom";
 
+import useTitle from "./hooks/use-title";
+
 function App() {
-  const [number1, setNumber1] = React.useState(0);
-  const [number2, setNumber2] = React.useState(0);
-
-  const func = () => alert("func");
-
-  // useEffect는 componentDidmount과 componentDidUpdate 역할을 함
-  React.useEffect(() => {
-    func();
-  }, [number2]);
-  // useEffect는 함수와 dependency를 인수로 받는다.
-
-  // component가 mount되는 즉시 func 실행하고
-  // dependency 배열 안의 값이 업데이트되면 실행
-
-  return (
-    <div>
-      <button onClick={() => setNumber1(number1 + 1)}>{number1}</button>
-      <button onClick={() => setNumber2(number2 + 1)}>{number2}</button>
-    </div>
-  );
+  const titleUpdater = useTitle("loading...");
+  setTimeout(() => titleUpdater("home") /* setTitle("home") */, 5000);
+  return <div></div>;
 }
 
 export default App;
---
+++ src/hooks/use-title.js
@@ -0,0 +1,14 @@
+function useTitle(initialTitle) {
+  const [title, setTitle] = React.useState(initialTitle);
+
+  function updateTitle() {
+    const htmlTitle = document.querySelector("title");
+    htmlTitle.innerText = title;
+  }
+
+  React.useEffect(() => updateTitle(), [title]);
+  // title의 값이 변경될 때 마다 updateTitle이 실행
+  return setTitle;
+}
+
+export default useTitle;
