593bd520650d8d38f4de7fdbff5dd0b8e97bdf77
khoon0608 <khoon0608@gmail.com>
2022-03-20 10:00:38 +0900

    useEffect 개념

--- src/App.js
+++ src/App.js
@@ -4,33 +4,27 @@ import { validate } from "json-schema";
 import React from "react";
 import ReactDOM from "react-dom";
 
-import useTabs from "./hooks/use-tabs";
-
-const content = [
-  {
-    tap: "Section 1",
-    content: "Section 1 content",
-  },
-  {
-    tap: "Section 2",
-    content: "Section 2 content",
-  },
-];
-
-
-
 function App() {
-  const { currentItem, changeItem } = useTabs(0, content);
+  const [number1, setNumber1] = React.useState(0);
+  const [number2, setNumber2] = React.useState(0);
+
+  const func = () => alert("func");
+
+  // useEffect는 componentDidmount과 componentDidUpdate 역할을 함
+  React.useEffect(() => {
+    func();
+  }, [number2]);
+  // useEffect는 함수와 dependency를 인수로 받는다.
+  
+  // component가 mount되는 즉시 func 실행하고
+  // number2의 값이 업데이트 될 때마다 실행
+
   return (
     <div>
-      {content.map((section, index) => (
-        <button onClick={() => changeItem(index)}>{section.tap}</button>
-      ))}
-      <div>{currentItem.content}</div>
+      <button onClick={() => setNumber1(number1 + 1)}>{number1}</button>
+      <button onClick={() => setNumber2(number2 + 1)}>{number2}</button>
     </div>
   );
 }
-
-// useInput을 사용하면 인풋의 입력을 다른 함수에서 처리할 수 있다.
 
 export default App;
